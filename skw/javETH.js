78505ca43535cdb487749380779f6c21200881d7
yogik23 <61941105+yogik23@users.noreply.github.com>
2024-10-31 20:22:27 +0700

    Update javETH.js

--- skw/javETH.js
+++ skw/javETH.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 const fs = require('fs');
 const chalk = require('chalk');
-const { displayskw1 } = require('./diskw');
+const axios = require('axios');
 
 const data = fs.readFileSync('dataSKW.json', 'utf-8');
 const credentials = JSON.parse(data);
@@ -40,66 +40,93 @@ async function startCD(seconds) {
     });
 }
 
+async function getETHPrice() {
+    try {
+        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+        return response.data.ethereum.usd;
+    } catch (error) {
+        console.error('Error mendapatkan harga ETH:', error);
+        return null;
+    }
+}
 
-async function javpayETH(numTransactions, amountETH, RPC_URL, network) {
-    console.clear();
-    displayskw1();
+async function javpayETH(numTransactions, amountETH, RPC_URL, network, SKWT) {
+    const ethPrice = await getETHPrice();
     const amountWei = web3.utils.toWei(amountETH.toFixed(18), 'ether');
     const contract = new web3.eth.Contract(contractABI, CONTRACT_ADDRESS);
     const nonces = {};
     web3.setProvider(new Web3.providers.HttpProvider(RPC_URL));
 
+    const gasPrice = await web3.eth.getGasPrice();
+    const GAS_LIMIT = 300000;
+    const MAX_PRIORITY_FEE_PER_GAS = web3.utils.toWei('0.05', 'gwei');
+    const MAX_FEE_PER_GAS = web3.utils.toWei('0.1', 'gwei');
 
     for (const key of privateKeys) {
         const account = web3.eth.accounts.privateKeyToAccount(key);
         nonces[account.address] = await web3.eth.getTransactionCount(account.address);
-        const balanceWei = await web3.eth.getBalance(account.address);
+        
+        let balanceWei;
+        try {
+            balanceWei = await web3.eth.getBalance(account.address);
+        } catch (error) {
+            console.log(`Error saat mendapatkan saldo untuk ${account.address}: ${error.message}`);
+            continue;
+        }
+
         const balanceETH = web3.utils.fromWei(balanceWei, 'ether');
+        const ETHUSD = (balanceETH * ethPrice).toFixed(4);
+        const depoUSD = (amountETH * ethPrice).toFixed(4);
 
         console.log();
         console.log(chalk.hex('#FFA500')(" ╔══════════════════════════════════════════════════════════════════╗"));
         console.log(chalk.hex('#32CD32')(` ║💀 Memproses Akun ${account.address}      `));
-        console.log(chalk.hex('#FFD700')(` ║💰 Saldo ETH :  ${balanceETH}      `));
+        console.log(chalk.hex('#FFD700')(` ║💰 Saldo ETH ${SKWT} :  ${balanceETH} ($${ETHUSD})     `));
         console.log(chalk.hex('#1E90FF')(` ║⚖️ Jumlah Transaksi: ${numTransactions}                                            `));
-        console.log(chalk.hex('#FF1493')(` ║💸 Amount yang dideposit: ${amountETH} ETH                            `));
+        console.log(chalk.hex('#FF1493')(` ║💸 Amount yang dideposit: ${amountETH} ETH ($${depoUSD})                           `));
         console.log(chalk.hex('#FFA500')(" ╚══════════════════════════════════════════════════════════════════╝"));
-
 
         for (let i = 0; i < numTransactions; i++) {
             const fromAddress = account.address;
             const shortFromAddress = `${fromAddress.slice(0, 4)}...${fromAddress.slice(-4)}`;
-            const gasLimit = 5000000;
-            const gasPrice = web3.utils.toWei('0.02', 'gwei'); 
+
+            const totalCost = parseFloat(amountWei) + (parseFloat(MAX_FEE_PER_GAS) * GAS_LIMIT);
+
+            if (balanceWei < totalCost) {
+                console.log(`Saldo tidak cukup untuk melakukan transaksi dari ${fromAddress}. Diperlukan: ${web3.utils.fromWei(totalCost.toString(), 'ether')} ETH`);
+                break;
+            }
 
             try {
                 console.log(chalk.hex('#1E90FF')(`\n🔄 Melakukan Tx ke ${i + 1}`));
-                const block = await web3.eth.getBlock('latest');
-                const baseFee = block.baseFeePerGas;
-
-                const maxFeePerGas = baseFee * 1.2; 
-                const maxPriorityFeePerGas = web3.utils.toWei('0.07', 'gwei');
                 const transaction = {
                     from: fromAddress,
                     to: CONTRACT_ADDRESS,
                     value: amountWei,
-                    gas: gasLimit,
-                    gasPrice: gasPrice,
+                    gas: GAS_LIMIT,
+                    maxFeePerGas: MAX_FEE_PER_GAS,
+                    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
                     nonce: nonces[fromAddress],
                     data: contract.methods.depositETH().encodeABI()
                 };
 
                 const signedTx = await web3.eth.accounts.signTransaction(transaction, key);
                 const txHash = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-                
+                const receipt = await web3.eth.getTransactionReceipt(txHash.transactionHash);
+                const totalCostInETH = (receipt.gasUsed * gasPrice) / 1e18;
+                const gasCostUSD = (totalCostInETH * ethPrice).toFixed(4);
+
+
                 const explorerUrl = network === '1' 
                     ? `https://basescan.org/tx/${txHash.transactionHash}`
                     : `https://arbiscan.io/tx/${txHash.transactionHash}`;
                 
                 console.log(chalk.hex('#00FF00')(`✅ Transaksi Berhasil`));
                 console.log(chalk.hex('#00FFFF')(`🔗 Rincian transaksi: ${explorerUrl}`));
+                console.log(chalk.hex('#FFB6C1')(`🧯 Gas yang digunakan: ${totalCostInETH.toFixed(7)} ETH ($${gasCostUSD})`));
                 
                 nonces[fromAddress] += 1;
-                await startCD(10)
+                await startCD(10);
                 console.log();
 
             } catch (e) {
@@ -114,6 +141,5 @@ async function javpayETH(numTransactions, amountETH, RPC_URL, network) {
         console.log(chalk.hex('#90ee90')(`Akun ${account.address} Selesai\n`));
     }
 }
-
 
 module.exports = { javpayETH };
